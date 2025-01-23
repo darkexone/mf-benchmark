685dfd67559d2b1bd9088e1cd384b947a66b6099
Hubert Krzeminski <darkexone@gmail.com>
2025-01-23 09:59:49 +0100

    Sync project and pnpm workspace

--- apps/app0/src/app/app.tsx
+++ apps/app0/src/app/app.tsx
@@ -1,22 +1,17 @@
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
+
+import { Screen0 } from '@mf-benchmark/lib0';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="app0" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
       <div role="navigation">
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/lib1">Lib1</Link>
           </li>
           <li>
             <Link to="/page-2">Page 2</Link>
@@ -33,6 +28,7 @@ export function App() {
             </div>
           }
         />
+        <Route path="/lib0" element={<Screen0 />} />
         <Route
           path="/page-2"
           element={
