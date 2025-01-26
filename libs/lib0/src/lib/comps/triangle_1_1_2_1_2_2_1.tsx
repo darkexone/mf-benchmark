12b734f663a43f429f9dc1026048f2d1f979a8ab
Hubert Krzeminski <darkexone@gmail.com>
2025-01-27 00:40:13 +0100

    Add Sierpinski triangle components

---
+++ libs/lib0/src/lib/comps/triangle_1_1_2_1_2_2_1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+interface TriangleProps {
+  style?: React.CSSProperties;
+}
+
+function Triangle({ style }: TriangleProps) {
+  return (
+    <>
+      <polygon points="-5,4.33 0,-4.33 5,4.33" style={style} />
+    </>
+  );
+}
+
+export default React.memo(Triangle);
